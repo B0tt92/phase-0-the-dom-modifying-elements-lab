5e995b2d38f2a06b9bc735405925e65298673611
+branden <+branden_ott@outlook.com+>
2021-10-03 17:30:52 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+//Write your code here!
+main.remove();
+const newHeader = document.createElement("h1");
+//document.body.append(newHeader);
+newHeader.id = 'victory';
+newHeader.innerHTML = "<h1> B0tt is the champion</h1>"
